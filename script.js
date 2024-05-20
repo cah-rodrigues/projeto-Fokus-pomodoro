7cbe27325d80d0948d565faa56c8c5b7c5ed4dc4
Carla Rodrigues <rodricah1.8@gmail.com>
2024-05-20 11:01:11 -0400

    configuracoes do timer

--- script.js
+++ script.js
@@ -1,33 +1,114 @@
-const html = document.querySelector('html');
-const focoBtn = document.querySelector('.app__card-button--foco');
-const curtobtn = document.querySelector('.app__card-button--curto');
-const longoBtn = document.querySelector('.app__card-button--longo');
+const html = document.querySelector('html')
 
-//variaveis da lista de exercícios ↓
-const startPauseBtn = document.querySelector('.app__card-primary-button');
-const timer = document.querySelector('.app__card-timer');
-const imagePosition = document.querySelector('.app__image');
-const contextPhrases = document.querySelector('.app__title');
+const banner = document.querySelector('.app__image') //imagem das páginas
+const contextPhrases = document.querySelector('.app__title') //frase das páginas
 
-const duracaoFoco = 1500;
-const duracaoDescansoCurto = 300;
-const duracaoDescansoLongo = 900;
+//variaveis dos buttons
+const buttons = document.querySelectorAll('.app__card-button')
+const startPauseBtn = document.querySelector('#start-pause')
+const focoBtn = document.querySelector('.app__card-button--foco')
+const curtobtn = document.querySelector('.app__card-button--curto')
+const longoBtn = document.querySelector('.app__card-button--longo')
+
+//variaveis do timer
+const timer = document.querySelector('.app__card-timer') //div do timer
+let duracaoTimer = 5; //25 min
+let intervaloId = null;
+
+//variaveis de sons
+const musicFocoInput = document.querySelector('#alternar-musica')
+const music = new Audio('/sons/luna-rise-part-one.mp3')
+music.loop = true;
 
 
 
+
+//Eventos e funções-----------------------------------------------------------------------------
+
+
+//evento do input de musica-----------------------------------------------------
+musicFocoInput.addEventListener('change', () =>{
+    if(music.paused) {
+        music.play()
+    }else {
+        music.pause()
+    }
+})
+
+//eventos dos botoes de foco e descanso----------------------------------
 focoBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
+    html.setAttribute('data-contexto', 'foco')
+    alterarContexto('foco')
+    focoBtn.classList.add('active')
 })
 
 curtobtn.addEventListener('click', () =>{
-    html.setAttribute('data-contexto', 'descanso-curto');
+    html.setAttribute('data-contexto', 'descanso-curto')
+    alterarContexto('descanso-curto')
+    curtobtn.classList.add('active')
 })
 
 longoBtn.addEventListener('click', () =>{
-    html.setAttribute('data-contexto','descanso-longo');
+    html.setAttribute('data-contexto', 'descanso-longo')
+    alterarContexto('descanso-longo')
+    longoBtn.classList.add('active')
 })
 
-//botões da lista de exercicios
+
+function alterarContexto(contexto){
+    buttons.forEach(function(contexto){
+        contexto.classList.remove('active')
+    }) //função para remover o 'active' dos botoes
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            contextPhrases.innerHTML = `
+            Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+
+            break;
+            case "descanso-curto":
+                contextPhrases.innerHTML = `
+                Que tal dar uma respirada?<br> 
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>
+                `
+
+            break;
+            case "descanso-curto":
+                contextPhrases.innerHTML = `
+                Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+                `
+    
+        default:
+            break;
+    }
+}
 
 
+const contagemRegressiva = () => {
+        if (duracaoTimer <= 0) {
+            zerar()
+            alert('Tempo finalizado!')
+            return 
+        }
+    duracaoTimer -= 1
+    console.log('Temporizador: ' + duracaoTimer)
+}
 
+startPauseBtn.addEventListener('click', initAndPause)
+
+function initAndPause() {
+    if(intervaloId){
+        zerar()
+        return
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
+}
